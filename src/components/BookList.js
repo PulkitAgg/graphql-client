85dce5d40cf6c044f6ef3a9d4545aa0657e1995c
Pulkit <pulkit@koolkanya.com>
2020-08-25 21:16:40 +0530

    Add book list screen

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react';
+import {http} from '../interceptor';
+
+const GET_BOOKS = `
+    {
+        books {
+            name,
+            id
+        }
+    }
+`;
 
 class BookList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        bookList: [],
+        isFetch: true
+    }
+}
+  componentDidMount() {
+    http
+      .post('', { query: GET_BOOKS})
+      .then(result => {
+        this.setState({
+          bookList: result.data.data.books,
+          isFetch: false
+        })
+      });
+  }
+
+  displayBooks(){
+    let {bookList, isFetch} = this.state;
+    if(isFetch){
+        return( <div>Loading books...</div> );
+    } else {
+        return bookList.map(book => {
+            return(
+                <li key={ book.id }>{ book.name } </li>
+            );
+        })
+    }
+}
     render(){
         return(
             <div>
                 <ul id="book-list">
-                    <li>Book name</li>
+                { this.displayBooks() }
                 </ul>
             </div>
         );
